f12d6c0272fd69fc541ee88502601fd2927d983d
Lucas Mendes <164028753+14lucas-mendes@users.noreply.github.com>
2025-08-01 21:39:18 -0300

    inicializada a estilização da NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
+import { HeartIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 export default function NavBar() {
     return (
-        <div className="flex justify-between items-center">
-            <Link href="/">
-                <Image src="/img/icons/logo.png" alt="logo" width={80} height={26} />
-            </Link>
+        <div className="flex justify-between items-center w-full h-16 bg-white mb-0.5 shadow-md">
             <div>
-                <Link href="/">HOME</Link>
+                <Link href="/">
+                    <Image src="/img/icons/logo.png" alt="logo" width={168} height={104} />
+                </Link>
+            </div>
+            <div className="flex items-center text-[12px] gap-16 font-extrabold">
+                <Link href="/" className="">HOME</Link>
                 <Link href="/phones">PHONES</Link>
                 <Link href="/contact">TABLETS</Link>
                 <Link href="/contact">ACCESSORIES</Link>
             </div>
-            <Link href="/favorites">
-                <Image src="/img/icons/heart_like.png" alt="heart" width={16} height={16} />
-            </Link>
-            <Link href="/cart">
-                <Image src="/img/icons/cart.png" alt="cart" width={16} height={16} />
-            </Link>
+            <div className="flex gap-11 items-center">
+                <Link href="/favorites">
+                    <HeartIcon className="w-6 h-6" />
+                </Link>
+                <Link href="/cart" className="mr-4">
+                    <ShoppingCartIcon className="w-6 h-6" />
+                </Link>
+            </div>
         </div>
     );
 }
