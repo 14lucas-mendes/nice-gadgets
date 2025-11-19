0d88d72809d087bc00a8a6c29bd183e323f45738
Lucas Mendes <164028753+14lucas-mendes@users.noreply.github.com>
2025-11-19 19:58:22 -0300

    feat: add ajuste de responsividade da home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <>
     <div className='py-6 px-4 sm:px-6 sm:py-8 sm:w-full md:w-full md:py-14 md:px-8 max-w-screen-xl mx-auto'>
-      
+     <h1 className='w-full font-extrabold tracking-tighter text-3xl'>Welcome to Nice Gadgets store!</h1>
     </div>
       <Slider />
       <Container>
